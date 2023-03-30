3c8d37d0f54153b204bfd90256f9167bc8c9c7d2
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-03-30 02:18:48 +0000

    [Update] ユーザー登録とログインの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
+  end
+
 end
