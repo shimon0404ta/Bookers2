8ac7b3af986ddc3a6d3f11e9cb7bee0a377ca26a
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-04-05 04:51:14 +0000

    [Finish] RSpecによるテスト完了

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2023_04_02_172731) do
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
